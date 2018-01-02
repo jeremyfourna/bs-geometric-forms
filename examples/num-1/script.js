84f6b98d09cdeeec0c47d8f0c3854c88943eb017
jeremyfourna <jeremyfourna@gmail.com>
2018-01-02 22:18:46 +0100

    WIP to improve README

---
+++ examples/num-1/script.js
@@ -0,0 +1,15 @@
+const { createRectangle } = require('../../index.js');
+
+const svg = d3.select('#app')
+  .append('svg');
+
+const xPosition = 0;
+const yPosition = 0;
+const width = 100;
+const height = 200;
+
+svg.attr('height', 400);
+svg.attr('width', 400);
+
+createRectangle(xPosition, yPosition, width, height, svg);
+createRectangle(width + xPosition, height + yPosition, width * 2, height, svg);
